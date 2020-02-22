bfc1fb0feb15cfe52c011b5e8c8c79084a3df7d6
LordNani <47566320+LordNani@users.noreply.github.com>
2020-02-22 10:54:48 +0200

    conflict for Anya

--- src/App.java
+++ src/App.java
@@ -6,12 +6,8 @@ public class App {
         int arr[] = {10, 7, 8, 9, 1, 5}; 
         int n = arr.length; 
         QuickSort obNew = new QuickSort(); 
-        QuickSort ob = new QuickSort(); 
-        ob.sort(arr, 0, n-1); 
-        QuickSort ab = new QuickSort();
-        QuickSort ac = new QuickSort();
-        QuickSort ar = new QuickSort();
-        
+        QuickSort object = new QuickSort(); 
+        object.sort(arr, 0, n-1); 
         System.out.println("sorted array"); 
         for(int i = 0; i < arr.length;i++) {
         	System.out.println(arr[i]); 
