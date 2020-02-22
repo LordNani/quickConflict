95d6da1d766e4ff8729353e2ef575c963525202f
LordNani <47566320+LordNani@users.noreply.github.com>
2020-02-22 10:24:12 +0200

    Tested quicksort

--- src/App.java
+++ src/App.java
@@ -1,9 +1,18 @@
-
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello world!");
+        int arr[] = {10, 7, 8, 9, 1, 5}; 
+        int n = arr.length; 
+  
+        QuickSort ob = new QuickSort(); 
+        ob.sort(arr, 0, n-1); 
+  
+        System.out.println("sorted array"); 
+        for(int i = 0; i < arr.length;i++) {
+        	System.out.println(arr[i]); 
+        }
 	}
 
 }
