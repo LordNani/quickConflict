9b022abb1805a9b90f83d427a33683bb2cebd9a1
Anna Kostiuk <anna.kostyuik.work@gmail.com>
2020-02-22 10:38:00 +0200

    Added new array

--- src/App.java
+++ src/App.java
@@ -8,7 +8,8 @@ public class App {
   
         QuickSort ob = new QuickSort(); 
         ob.sort(arr, 0, n-1); 
-  
+        QuickSort ab = new QuickSort();
+        
         System.out.println("sorted array"); 
         for(int i = 0; i < arr.length;i++) {
         	System.out.println(arr[i]); 
