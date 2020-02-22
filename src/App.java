ffdd6bff786b47f4c2ba7b9212c78fcd908ca3f2
Anna Kostiuk <anna.kostyuik.work@gmail.com>
2020-02-22 10:47:14 +0200

    Changed the name of the array

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App {
         int arr[] = {10, 7, 8, 9, 1, 5}; 
         int n = arr.length; 
   
-        QuickSort ob = new QuickSort(); 
+        QuickSort array = new QuickSort(); 
         ob.sort(arr, 0, n-1); 
         QuickSort ab = new QuickSort();
         
